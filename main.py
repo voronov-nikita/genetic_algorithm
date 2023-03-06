8fe01664e6c3a534720483980e696e16905d048a
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-03-06 23:20:58 +0300

    progress_one

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello")
+import random
+
+
+def mutation(list_gen:list):
+    for i in list_gen:
+        if round(random.random()):
+            i = not(i)
+
+
+class Person:
+    def __init__(self):
+        self.gen = []
+
+
+person = Person()
+        
